36d6638c96caebd72f59b03696992f06eb3bb67a
idobas <ido.basch@sap.com>
2017-02-18 18:50:42 +0200

    fixed small bug

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,4 @@
-var sentencesArray = [
+let sentencesArray = [
     "Wow, impressive stuff! keep up the good work!",
     "Seems like your willpower is strong, don't give up!",
     "Don't worry, you aren't missing anything...",
@@ -6,7 +6,7 @@ var sentencesArray = [
     "You're almost in the Guinness world records, great job!"
 ]
 
-var imagesArray = [
+let imagesArray = [
     "images/happy.jpg",
     "images/happy2.jpg",
     "images/happy3.jpg",
@@ -15,14 +15,14 @@ var imagesArray = [
 ]
 
 function createAndAppendElement(text, element) {
-    var newElement = document.createElement(element);
-    var text = document.createTextNode(text);
-    newElement.appendChild(text);
+    let newElement = document.createElement(element);
+    let textNode = document.createTextNode(text);
+    newElement.appendChild(textNode);
     document.body.appendChild(newElement);
 }
 
 function createAndAppendImage(imageSrc) {
-    var oImg = document.createElement("img");
+    let oImg = document.createElement("img");
     oImg.setAttribute('src', imageSrc);
     document.body.appendChild(oImg);
 }
@@ -32,49 +32,61 @@ function generateRandomFromArray(array) {
 }
 
 function generateRandomSentence() {
-  return generateRandomFromArray(sentencesArray);
+    return generateRandomFromArray(sentencesArray);
 }
 
 function generateRandomImage() {
-  return generateRandomFromArray(imagesArray);
+    return generateRandomFromArray(imagesArray);
+}
+
+function lessThanAnHour() {
+    createAndAppendElement("That is less than 1 hour - too much facebook!", "h4");
+    createAndAppendImage("images/angry.png");
+}
+
+function calculateTimes(visits) {
+    let currentTime = new Date().getTime();
+    let latestVisit = visits[visits.length - 1].visitTime;
+    let timeFromLastVisit = currentTime - latestVisit;
+    let timeFromLastVisitInMinutes = Math.floor((timeFromLastVisit / 60000));
+    let timeFromLastVisitInHours = timeFromLastVisitInMinutes / 60;
+    let timeFromLastVisitInMinutesRemain = (timeFromLastVisitInHours % 1) * 60;
+    return {
+        timeFromLastVisitInHours: Math.floor(timeFromLastVisitInHours),
+        timeFromLastVisitInMinutesRemain: Math.floor(timeFromLastVisitInMinutesRemain)
+    };
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     chrome.history.getVisits({
         url: "https://www.facebook.com/"
-    }, function(visits) {
-        var visitsLength = visits.length >= 1;
+    }, visits => {
+        let visitsLength = visits.length >= 1;
         if (visits && visitsLength) {
-            var currentTime = new Date().getTime();
-            var latestVisit = visits[visits.length - 1].visitTime;
-            var timeFromLastVisit = currentTime - latestVisit;
-            var timeFromLastVisitInMinutes = Math.floor((timeFromLastVisit / 60000));
-            var timeFromLastVisitInHours = timeFromLastVisitInMinutes / 60;
-            var timeFromLastVisitInMinutesRemain = (timeFromLastVisitInHours % 1) * 60;
-            createAndAppendElement(Math.floor(timeFromLastVisitInHours) + " Hours, " + Math.floor(timeFromLastVisitInMinutesRemain) + " minutes", "h3");
-            if (Math.floor(timeFromLastVisitInHours) === 0) {
-                createAndAppendElement("That is less than 1 hour - too much facebook!", "h4");
-                createAndAppendImage("angry.png");
+            let times = calculateTimes(visits);
+            createAndAppendElement(`${times.timeFromLastVisitInHours} Hours, ${times.timeFromLastVisitInMinutesRemain} Minutes`, "h3");
+            if (times.timeFromLastVisitInHours === 0) {
+                lessThanAnHour();
             } else {
-                var hours = localStorage.getItem("hours");
-                var sentence = localStorage.getItem("sentence");
-                var image = localStorage.getItem("image");
+                let hours = localStorage.getItem("hours");
+                let sentence = localStorage.getItem("sentence");
+                let image = localStorage.getItem("image");
                 if (!image) {
-                  image = generateRandomImage();
-                  localStorage.setItem("image", image);
+                    image = generateRandomImage();
+                    localStorage.setItem("image", image);
                 }
                 if (!hours) {
-                  hours = Math.floor(timeFromLastVisitInHours);
-                  localStorage.setItem("hours", hours);
+                    hours = times.timeFromLastVisitInHours;
+                    localStorage.setItem("hours", hours);
                 }
                 if (!sentence) {
-                  sentence = generateRandomSentence();
-                  localStorage.setItem("sentence", sentence);
+                    sentence = generateRandomSentence();
+                    localStorage.setItem("sentence", sentence);
                 }
-                if (parseInt(hours) !== Math.floor(timeFromLastVisitInHours)) {
+                if (parseInt(hours) !== times.timeFromLastVisitInHours) {
                     sentence = generateRandomSentence();
                     image = generateRandomImage();
-                    localStorage.setItem("hours", Math.floor(timeFromLastVisitInHours));
+                    localStorage.setItem("hours", times.timeFromLastVisitInHours);
                     localStorage.setItem("sentence", sentence);
                     localStorage.setItem("image", image);
                     createAndAppendElement(sentence, "h4");
@@ -86,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         } else {
             createAndAppendElement("You haven't visited facebook since the begining of time!", "h3");
-            createAndAppendImage("happy.jpg");
+            createAndAppendImage("images/happy.jpg");
         }
     });
 });
